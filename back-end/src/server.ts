d0bcf72cceef37a8d5d4fc38ee95185a44a7488a
PedroMakengo <pedromakengomiguel@gmail.com>
2022-07-03 18:14:39 +0100

    Criando as minhas rotas

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,1 +1,23 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+
+app.get("/users", (request, response) => {
+  return response.json({
+    users: ["Usuário 1", "Usuario 2"],
+  });
+});
+
+app.post("/users", (request, response) => {
+  return response.json({ message: "Criando usuário" });
+});
+app.put("/users", (request, response) => {
+  return response.json({ message: "Atualizando usuário" });
+});
+app.delete("/users", (request, response) => {
+  return response.json({ message: "Excluindo usuário" });
+});
+
+app.listen("3333", () => {
+  console.log("Back-end Started !");
+});

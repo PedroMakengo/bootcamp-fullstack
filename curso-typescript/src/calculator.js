26bc0dda5695c789f14213d8f7e555357ebd6a11
PedroMakengo <pedromakengomiguel@gmail.com>
2022-07-03 16:00:30 +0100

    Trabalhando com decorators

--- curso-typescript/src/calculator.js
+++
@@ -1,3 +0,0 @@
-export const sum = (a, b) => a + b;
-
-export const subtraction = (a, b) => a - b;
---
+++ curso-typescript/src/controllers/http/Users.ts
@@ -0,0 +1,5 @@
+import axios from "@/utils/axios";
+
+export default () => {
+  console.log("Users");
+};

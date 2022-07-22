20b43255de8b2dfc289c14deac8d409473e108c7
PedroMakengo <pedromakengomiguel@gmail.com>
2022-07-22 21:43:31 +0100

    Criando a minha molecula

--- 06-atomic-design/02-atomic-design/src/App.vue
+++ 06-atomic-design/02-atomic-design/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="component">
-    <NavBar />
+    <SearchForm />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { NavBar } from "@/components/molecules";
+import { SearchForm } from "@/components/molecules";
 
 export default defineComponent({
   components: {
-    NavBar,
+    SearchForm,
   },
 });
 </script>
--- 06-atomic-design/02-atomic-design/src/components/molecules/index.ts
+++ 06-atomic-design/02-atomic-design/src/components/molecules/index.ts
@@ -1,1 +1,2 @@
 export { default as NavBar } from "./NavBar.vue";
+export { default as SearchForm } from "./SearchForm.vue";

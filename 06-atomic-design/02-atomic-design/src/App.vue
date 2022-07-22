d45905a4bab04079097171e097941d4bd85bac88
PedroMakengo <pedromakengomiguel@gmail.com>
2022-07-22 04:09:51 +0100

    Criando os meus primeiros atomos

--- 06-atomic-design/02-atomic-design/src/App.vue
+++ 06-atomic-design/02-atomic-design/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1 class="">Holla Mundo</h1>
+  <div class="component">
+    <NavBar />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { NavBar } from "@/components/molecules";
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    NavBar,
+  },
+});
 </script>
+
+<style lang="scss" scoped></style>
---
+++ 06-atomic-design/02-atomic-design/src/components/atoms/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src="@/assets/images/logo.svg" class="Marcos" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  width: 100%;
+}
+</style>

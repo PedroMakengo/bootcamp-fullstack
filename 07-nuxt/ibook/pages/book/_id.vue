c84b27b47a50b0a896c318325a03cf3cfbe46570
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-04 20:08:55 +0100

    Pegando um dado de um determinado usuário

---
+++ 07-nuxt/ibook/pages/book/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ $route.params.id }}</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '~/store'
+
+export default Vue.extend({
+  async asyncData({ params }) {
+    await books.show({ id: params.id as any })
+    console.log(books)
+  },
+})
+</script>
+
+<style scoped></style>

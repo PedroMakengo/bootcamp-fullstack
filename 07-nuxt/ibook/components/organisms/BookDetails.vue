d555dd5ac0f873c22ebca5df29357712b27caffa
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-04 20:23:13 +0100

    Adicionando as minhas moleculas

---
+++ 07-nuxt/ibook/components/organisms/BookDetails.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="book-details">
+    <BookCover />
+    <BookInfo />
+  </div>
+</template>
+
+<style scoped></style>
--- 07-nuxt/ibook/pages/book/_id.vue
+++ 07-nuxt/ibook/pages/book/_id.vue
@@ -4,13 +4,13 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { books } from '~/store'
+// import { books } from '~/store'
 
 export default Vue.extend({
-  async asyncData({ params }) {
-    await books.show({ id: params.id as any })
-    console.log(books)
-  },
+  // async asyncData({ params }) {
+  //   await books.show({ id: params.id as any })
+  //   console.log(books)
+  // },
 })
 </script>
 

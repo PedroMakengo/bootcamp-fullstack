0654cbdf30782f407d555e1b07684e396ddcc9ed
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-11 15:01:31 +0100

    Trabalhando

---
+++ 08-introducao-adonis/backend/app/Controllers/Http/PostsController.ts
@@ -0,0 +1,8 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PostsController {
+  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+  index() {
+    return ['post 1', 'post 2', 'post 3']
+  }
+}

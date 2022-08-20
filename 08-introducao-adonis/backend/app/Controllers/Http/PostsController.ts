1037fcd5b0d485d11c4c761491e0a0e575623ee0
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-20 09:23:37 -0300

    Trabalhando

--- 08-introducao-adonis/backend/app/Controllers/Http/PostsController.ts
+++ 08-introducao-adonis/backend/app/Controllers/Http/PostsController.ts
@@ -1,15 +1,37 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
 export default class PostsController {
-  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-  index() {
-    return [
-      {
-        id: 1,
-        name: 'Pedro',
-        idade: 23,
-        peso: '90k',
-      },
-    ]
+  public async index({}: HttpContextContract) {
+    const posts = await Post.all()
+
+    return posts
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content'])
+    const post = await Post.create(data)
+
+    return post
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+
+    return post
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const data = request.only(['title', 'content'])
+
+    post.merge(data)
+    await post.save()
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+
+    await post.delete()
   }
 }

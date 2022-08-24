ab037a7d5f9d69620ec97fe30cfd12816864b434
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-24 04:06:17 -0300

    Criando as minhas seeders

--- 08-introducao-adonis/backend/database/migrations/1587988332388_users.ts
+++ 08-introducao-adonis/backend/database/migrations/1587988332388_users.ts
@@ -8,6 +8,7 @@ export default class UsersSchema extends BaseSchema {
       table.increments('id').primary()
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
+      table.enu('role', ['normal', 'admin']).defaultTo('normal')
       table.string('remember_me_token').nullable()
       table.timestamps(true)
     })
---
+++ 08-introducao-adonis/backend/database/seeders/CreateUsers.ts
@@ -0,0 +1,19 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class CreateUsers extends BaseSeeder {
+  public async run() {
+    await User.createMany([
+      {
+        email: 'admin@gmail.com',
+        password: 'secret',
+        role: 'admin',
+      },
+      {
+        email: 'normal@gmail.com',
+        password: 'secret$17',
+        role: 'normal',
+      },
+    ])
+  }
+}

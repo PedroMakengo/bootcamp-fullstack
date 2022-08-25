ecd50a1f0c62e6c9674c5289610aea3b5974cbc5
PedroMakengo <pedromakengomiguel@gmail.com>
2022-08-25 09:50:38 -0300

    Terminando o módulo

--- 08-introducao-adonis/backend/app/Controllers/Http/PostsController.ts
+++ 08-introducao-adonis/backend/app/Controllers/Http/PostsController.ts
@@ -4,7 +4,8 @@ import { StoreValidator, UpdateValidator } from 'App/Validators/Post'
 
 export default class PostsController {
   public async index({}: HttpContextContract) {
-    const posts = await Post.query().orderBy('id').preload('author')
+    // const posts = await Post.query().orderBy('id').preload('author')
+    const posts = await Post.query().paginate(1, 1)
 
     return posts
   }

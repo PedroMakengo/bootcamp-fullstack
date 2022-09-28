e9fe9fcf22ad90c7a13d9229555fda1673735775
PedroMakengo <pedromakengomiguel@gmail.com>
2022-09-28 17:52:00 +0000

    Adicionando nova rota

--- 09-backend-facebook/backend/start/routes/auth.ts
+++ 09-backend-facebook/backend/start/routes/auth.ts
@@ -1,4 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('/auth', 'Auth/Main.store')
-Route.delete('/auth', 'Auth/Main.store').middleware('auth')
+// Route.post('/auth', 'Auth/Main.store')
+// Route.delete('/auth', 'Auth/Main.store').middleware('auth')
+
+Route.resource('/auth', 'Auth/Main')
+  .only(['store', 'destroy'])
+  .middleware({
+    destroy: ['auth'],
+  })
